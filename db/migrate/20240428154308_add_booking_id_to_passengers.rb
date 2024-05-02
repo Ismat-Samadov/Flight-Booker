166d9a209f7b54a4647b1843732e5326f7fc28c7
Ismat-Samadov <ismetsemedov@gmail.com>
2024-05-03 01:47:01 +0400

    clear

--- db/migrate/20240428154308_add_booking_id_to_passengers.rb
+++
@@ -1,5 +0,0 @@
-class AddBookingIdToPassengers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :passengers, :booking_id, :integer
-  end
-end
--- db/migrate/20240428161338_add_num_tickets_to_bookings.rb
+++
@@ -1,5 +0,0 @@
-class AddNumTicketsToBookings < ActiveRecord::Migration[7.1]
-  def change
-    add_column :bookings, :num_tickets, :integer
-  end
-end
